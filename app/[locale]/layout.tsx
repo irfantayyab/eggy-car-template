022ac1b70893bd146aaa1f98d83075e7313fc754
Irfan <irfantayyib07@gmail.com>
2025-10-19 18:49:53 +0500

    Added analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Aleo } from "next/font/google";
 import "../globals.css";
@@ -43,6 +44,7 @@ export default async function RootLayout({
      <main className="flex-stretch bg-white p-10">{children}</main>
      <Footer />
     </NextIntlClientProvider>
+    <Analytics />
    </body>
   </html>
  );
